4ba711cc0fbff5dcfc2cc1ef7d76eb690e4d88dc
Jiwon <winning1993@naver.com>
2022-05-01 22:19:59 +0900

    [ADD] 유저 카메라 목록 불러오기 기능 구현

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -3,10 +3,28 @@ const socket = io();
 const myFace = document.getElementById("myFace");
 const muteBtn = document.getElementById("mute");
 const cameraBtn = document.getElementById("camera");
+const camerasSelect = document.getElementById("cameras");
 
 let myStream;
 let muted = false;
 let cameraOff = false;
+
+async function getCameras() {
+    try {
+        const devices = await navigator.mediaDevices.enumerateDevices();
+        const cameras = devices.filter(device => device.kind === "videoinput");
+        cameras.forEach(camera => {
+            const option = document.createElement("option");
+            option.value = camera.deviceId;
+            option.innerText = camera.label;
+            camerasSelect.appendChild(option);
+        })
+        console.log(cameras);
+    }
+    catch (e) {
+        console.log(e)
+    }
+}
 
 async function getMedia() {
     try {
@@ -23,6 +41,7 @@ async function getMedia() {
 }
 
 getMedia();
+getCameras();
 
 function handleMuteClick() {
     myStream
@@ -39,6 +58,9 @@ function handleMuteClick() {
 }
 
 function handleCameraClick() {
+    myStream
+    .getVideoTracks()
+    .forEach((track) => (track.enabled = !track.enabled));
     if(cameraOff) {
         cameraBtn.innerText = "Turn Camera Off";
         cameraOff = false;
